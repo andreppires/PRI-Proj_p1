69d761674749f5e8a60fece6b50e02108df37083
andreppires <appires1993>
2016-11-04 21:34:30 +0000

    3-1:Clean version of the module. Applyed now for the 20newsgroups! Carrega Benfica!

--- exercise-3_1.py
+++ exercise-3_1.py
@@ -1,7 +1,7 @@
+from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import TfidfVectorizer
 from stop_words import get_stop_words
 import operator
-import os
 import nltk
 
 # get stop words
@@ -10,14 +10,9 @@ stop_words = get_stop_words('en')
 # user must select document to test min=1!
 docTest = 1
 
-# fetching train data/collenction from disk
-# using wiki20 collection from https://github.com/zelandiya/keyword-extraction-datasets
-train = []
-for file in os.listdir("wiki20/documents"):
-    if file.endswith(".txt"):
-        with open("wiki20/documents/" + file, 'r') as myfile:
-            train.append(myfile.read().replace('\n', ''))
-
+#fetch train and test data
+train = fetch_20newsgroups(subset='train')
+test = fetch_20newsgroups(subset='test')
 
 # In this case we only wanna some patterns of tokens
 #   JJ-NN-IN
@@ -27,7 +22,7 @@ for file in os.listdir("wiki20/documents"):
 newtrain=[]
 flag1=False
 flag2=False
-for doc in train:
+for doc in train.data[:100]:
     newwords = nltk.word_tokenize(doc)
     token = nltk.pos_tag(newwords)
     newdoc=''
@@ -52,15 +47,14 @@ for doc in train:
     flag1=False
     flag2=False
 
-# we do not want to recalculate idf and we want words and word bi-grams
+# we do not want to recalculate idf and we want words and word bi-grams or tri-gramas
 vectorizer = TfidfVectorizer(use_idf=False, ngram_range=(1,3), stop_words=stop_words)
 
-print newtrain
 # learn idf
 trainvec = vectorizer.fit_transform(newtrain)
 
 # input document as array
-input_document = train[docTest - 1:docTest]
+input_document = test.data[:1]
 
 # apply tf
 testvec = vectorizer.transform(input_document)
@@ -79,30 +73,6 @@ for i in testvec.nonzero()[1]:
 # sort tuples by weight
 sorted_name_weight = sorted(name_weight.items(), key=operator.itemgetter(1))
 
-print "VAI!"
-print sorted_name_weight
 # show top 5 weights (most 'heavy')
 for i in range(0, 5):
     print sorted_name_weight[i][0] + ' : ' + str(sorted_name_weight[i][1])
-
-# fetching result data/collection from disk
-# using wiki20 collection from https://github.com/zelandiya/keyword-extraction-datasets
-humanResult = []
-file = os.listdir("wiki20/teams/team1")
-with open("wiki20/teams/team1/" + file[docTest - 1], 'r') as myfile:
-    for line in myfile:
-        aux = line.split(":")
-        aux = aux[1].split("(")
-        aux[0] = aux[0][1:]
-        if (aux[0].endswith(" ")) or (aux[0].endswith("\n")):
-            aux[0] = aux[0][:-1]
-        humanResult.append(aux[0].lower())
-
-tdidfresult = []
-for i in sorted_name_weight[: len(humanResult)]:
-    tdidfresult.append(str(i[0]).lower())
-print "PREDICT"
-print tdidfresult
-print "REAL"
-print humanResult
-
